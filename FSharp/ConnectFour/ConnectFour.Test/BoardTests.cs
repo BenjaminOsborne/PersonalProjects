521859d6371f508a102765904af0813a10e23b54
Benjamin Osborne <ben.i.osborne@gmail.com>
2016-12-20 17:57:28 +0000

    Usings and build

--- FSharp/ConnectFour/ConnectFour.Test/BoardTests.cs
+++ FSharp/ConnectFour/ConnectFour.Test/BoardTests.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using ConnectFour.Engine;
 using ConnectFour.Runner;
-using Microsoft.FSharp.Core;
 using NUnit.Framework;
 
 namespace ConnectFour.Test
