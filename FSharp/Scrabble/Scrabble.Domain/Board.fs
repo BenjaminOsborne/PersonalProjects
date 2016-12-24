e3e322f522e07ecd034614d9e86a8e2250f8ee28
Benjamin Osborne <ben.i.osborne@gmail.com>
2016-12-24 17:40:09 +0000

    Completed initialisation

--- FSharp/Scrabble/Scrabble.Domain/Board.fs
+++ FSharp/Scrabble/Scrabble.Domain/Board.fs
@@ -43,21 +43,27 @@ type Board(tileLocations : TileLocation[,], Width:int, Height:int) =
 
 type BoardCreator = 
     static member Default =
-        let size = 13
+        let size = 15
         let create w h space = { Location = { Width = w; Height = h }; State = TileState.Free(space) }
         let array = Array2D.init size size (fun x y -> create x y None)
         let apply(tiles: seq<TileLocation>) = tiles |> Seq.iter (fun x -> array.[x.Location.Width, x.Location.Height] <- x);
         
+        let coMap2 indW indH = indW |> Seq.map (fun w -> indH |> Seq.map (fun h -> (w,h)));
+        let coMap ind = coMap2 ind ind;
+        let coMapRev indA indB = Seq.append (coMap2 indA indB) (coMap2 indB indA);
+        
         let createFromRange indexes state = indexes |> Seq.collect (fun x -> x) |> Seq.map (fun (w,h) -> create w h state)
 
-        let tripple = createFromRange ({0..6..size} |> Seq.map (fun w -> {0..6..size} |> Seq.map (fun h -> (w,h))))
-                                      (WordMultiply 3)
-        let double = createFromRange ({1..4} |> Seq.map (fun n -> [ (n,n); (n, size-1-n); (size-1-n,n); (size-1-n,size-1-n)] ))
+        let tripWrd = createFromRange (coMap {0..7..size}) (WordMultiply 3)
+        let dubWrd = createFromRange ({1..4} |> Seq.map (fun n -> [ (n,n); (n, size-1-n); (size-1-n,n); (size-1-n,size-1-n)] ))
                                      (WordMultiply 2)
-        let start = [ create 6 6 (WordMultiply 2) ]
+        let start = [7] |> Seq.map (fun x -> create x x (WordMultiply 2))
 
-        apply tripple
-        apply double
-        apply start
-         
-        new Board(array, 13, 13);
+        let trpLet = createFromRange (coMap [1;5;9;13]) (LetterMultiply 3)
+        
+        let dubLetSeq = Seq.append (coMapRev [0;7;14] [3;11]) (coMap [2;6;8;12]);
+        let dubLet = createFromRange dubLetSeq (LetterMultiply 2)
+        
+        apply ([trpLet; dubLet; tripWrd; dubWrd; start] |> Seq.collect (fun x -> x))
+        
+        new Board(array, size, size);
