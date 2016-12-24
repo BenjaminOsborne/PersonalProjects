8050896a25d47c5d0d74262140abfd9a5ed8dc2d
Benjamin Osborne <ben.i.osborne@gmail.com>
2016-12-24 19:37:06 +0000

    added test

---
+++ FSharp/Scrabble/Scrabble.Domain.Tests/BoardTests.fs
@@ -0,0 +1,17 @@
+﻿module BoardTests
+
+open NUnit.Framework
+open FsUnit
+open Scrabble.Domain
+
+let coMap2 a b = SequenceHelpers.CoMap2 a b
+
+[<Test>]
+let ``When empty board``() = 
+    let board = Board.Empty 2 3
+    coMap2 [0..1] [0..2]
+        |> Seq.iter (fun (w, h) ->
+            let tile = board.TileAt w h
+            tile.State |> should equal (Free None))
+    
+
--- FSharp/Scrabble/Scrabble.Domain/Board.fs
+++ FSharp/Scrabble/Scrabble.Domain/Board.fs
@@ -1,5 +1,9 @@
 ﻿namespace Scrabble.Domain
 
+type SequenceHelpers = 
+    static member CoMap2 indW indH = indW |> Seq.map (fun w -> indH |> Seq.map (fun h -> (w,h))) |> Seq.collect (fun x -> x);
+    static member CoMap ind = SequenceHelpers.CoMap2 ind ind;
+
 type Location = { Width : int; Height: int}
 
 type Letter = | Letter of char
@@ -29,12 +33,18 @@ type Board(tileLocations : TileLocation[,], Width:int, Height:int) =
                                            | LetterMultiply(m) -> "L" + m.ToString() + ""
                                            | WordMultiply(m) -> "W" + m.ToString() + ""
 
+    static member Empty width height =
+        let array = Array2D.init width height (fun w h -> { Location = { Width = w; Height = h }; State = TileState.Free(None) })
+        new Board(array, width, height)
+
     member this.Play(tiles : TilePlay list) =
         let state = tileLocations |> Array2D.copy
         for tile in tiles do
             state.[tile.Location.Width, tile.Location.Height] <- { Location = tile.Location; State = TileState.Played tile.Piece }
         new Board(state, Width, Height)
 
+    member this.TileAt width height = tileLocations.[width, height]
+    
     override this.ToString() = 
         let rows = {Height-1 .. -1 .. 0} |> Seq.map
                     (fun h -> {0 .. Width-1} |> Seq.map (fun w -> tileLocations.[w,h] |> tileToString)
@@ -48,15 +58,15 @@ type BoardCreator =
         let array = Array2D.init size size (fun x y -> create x y None)
         let apply(tiles: seq<TileLocation>) = tiles |> Seq.iter (fun x -> array.[x.Location.Width, x.Location.Height] <- x);
         
-        let coMap2 indW indH = indW |> Seq.map (fun w -> indH |> Seq.map (fun h -> (w,h)));
-        let coMap ind = coMap2 ind ind;
+        let coMap2 = SequenceHelpers.CoMap2
+        let coMap = SequenceHelpers.CoMap
         let coMapRev indA indB = Seq.append (coMap2 indA indB) (coMap2 indB indA);
         
-        let createFromRange indexes state = indexes |> Seq.collect (fun x -> x) |> Seq.map (fun (w,h) -> create w h state)
+        let createFromRange indexes state = indexes |> Seq.map (fun (w,h) -> create w h state)
 
         let tripWrd = createFromRange (coMap {0..7..size}) (WordMultiply 3)
         
-        let dubWrdSeq = {1..4} |> Seq.map (fun n -> [ (n,n); (n, size-1-n); (size-1-n,n); (size-1-n,size-1-n)] );
+        let dubWrdSeq = {1..4} |> Seq.map (fun n -> [ (n,n); (n, size-1-n); (size-1-n,n); (size-1-n,size-1-n)] ) |> Seq.collect (fun x -> x);
         let dubWrd = createFromRange dubWrdSeq (WordMultiply 2)
 
         let start = [7] |> Seq.map (fun x -> create x x (WordMultiply 2))
