d949e0a6a16ad509a42e2a5b2cc4f60fe744142d
Benjamin Osborne <ben.i.osborne@gmail.com>
2016-12-29 21:50:31 +0000

    Added tests

--- FSharp/Scrabble/Scrabble.Domain.Tests/PossibleWordPlayTests.fs
+++ FSharp/Scrabble/Scrabble.Domain.Tests/PossibleWordPlayTests.fs
@@ -6,15 +6,20 @@ open Scrabble.Domain
 
 let getPossible board tileHand wordSet = (new BoardSpaceAnalyser()).GetPossibleScoredPlays board tileHand wordSet
 
-[<Test>]
-let ``Word is possible``() =
-    let assertSome words letters (assertData: int list) =
-        let board = Board.Empty 3 3
-        let tiles = letters |> Seq.map (fun c -> { Letter = c; Value = 0}) |> Seq.toList
-        let tileHand = new TileHand(tiles)
-        let wordSet = new WordSet(words |> Set)
-        let possible = getPossible board tileHand wordSet |> Seq.toArray
-        possible.Length |> should equal assertData.Length
-        assertData |> Seq.iteri (fun nx x -> possible.[nx].Words.Length |> should equal x)
+let assertSome board words letters (assertData: int list) =
+    let tiles = letters |> Seq.map (fun c -> { Letter = c; Value = 0}) |> Seq.toList
+    let tileHand = new TileHand(tiles)
+    let wordSet = new WordSet(words |> Set)
+    let possible = getPossible board tileHand wordSet |> Seq.toArray
+    possible.Length |> should equal assertData.Length
+    assertData |> Seq.iteri (fun nx x -> possible.[nx].Words.Length |> should equal x)
 
-    assertSome ["tie"] ['t'; 'i'; 'e'] [1; 1]
+[<Test>]
+let ``With empty board``() =
+    let board = Board.Empty 3 3
+    assertSome board ["tie"] [] []
+    assertSome board ["tie"] ['t'; 'i'; 'f'] []
+
+    assertSome board ["tie"] ['t'; 'i'; 'e'] [1; 1]
+    
+    assertSome board ["dog"; "god"] ['o'; 'd'; 'g'] [2; 2]
