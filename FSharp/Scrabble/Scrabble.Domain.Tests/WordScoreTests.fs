5cbeecaaf8e08ef7005229708316afad4c4033fb
BenjaminOsborne <ben@accurx.com>
2017-01-02 19:05:51 +0000

    Updated tests

--- FSharp/Scrabble/Scrabble.Domain.Tests/WordScoreTests.fs
+++ FSharp/Scrabble/Scrabble.Domain.Tests/WordScoreTests.fs
@@ -4,47 +4,51 @@ open NUnit.Framework
 open FsUnit
 open Scrabble.Domain
 
+let loadedWordSet = Lazy.Create(WordLoader.LoadAllWords)
+
 let getPossible board tileHand wordSet = (new BoardSpaceAnalyser()).GetPossibleScoredPlays board tileHand wordSet
 
-let assertScoreTiles board (word:string) (letters:seq<char>) score =
+let assertScoreTiles board wordSet (word:string) (letters:seq<char>) score =
     let tileBag = TileBagCreator.Default
     let tiles = letters |> Seq.map (fun c -> let (_,t) = tileBag.DrawFromLetter(BagTileLetter.Letter(c))
                                              { Letter = c; Value = t.Value })
                         |> Seq.toList
     let tileHand = new TileHand(tiles)
-    let wordSet = new WordSet([word] |> Set)
     let possible = getPossible board tileHand wordSet |> Seq.toList
     possible.Length |> should greaterThanOrEqualTo 1
     
     let best = possible.Head.WordScores.Head
     best.Word.Word |> should equal word
     best.Score |> should equal score
-    
-    possible |> Seq.iter (fun t ->
-        t.WordScores.Length |> should equal 1
-        t.WordScores.Head.Score |> should lessThanOrEqualTo best.Score
-        )
+    possible
 
-let assertScore board (word:string) score = assertScoreTiles board word word score
+let assertScoreSingleWord board (word:string) score =
+    let wordSet = new WordSet([word] |> Set)
+    let letters = word
+    let possible = assertScoreTiles board wordSet word letters score
+    possible |> Seq.iter (fun t -> t.WordScores.Length |> should equal 1
+                                   t.WordScores.Head.Score |> should lessThanOrEqualTo score)
 
 let assertPlayArray array word tiles score =
     let initial = BoardCreator.Default
     let board = BoardCreator.PlayArray initial array
-    assertScoreTiles board word tiles score
+    let wordSet = loadedWordSet.Value
+    let possible = assertScoreTiles board wordSet word tiles score
+    possible |> ignore
 
 [<Test>]
 let ``With empty board``() =
     let board = Board.Empty 3 1
-    assertScore board "tie" 3 // (1+1+1)
+    assertScoreSingleWord board "tie" 3 // (1+1+1)
 
 [<Test>]
 let ``With default board``() =
     let board = BoardCreator.Default
     
-    assertScore board "a" 2 //(1*2)
-    assertScore board "avise" 18 //((1*2) + 4 + 1 + 1 + 1) * 2
-    assertScore board "aaeeiioo" 54 //(7 + 1*2) * 2 * 3 -> 54
-    assertScore board "farces" 30 //((4*2) + 1 + 1 + 3 + 1 + 1) * 2 -> 30
+    assertScoreSingleWord board "a" 2 //(1*2)
+    assertScoreSingleWord board "avise" 18 //((1*2) + 4 + 1 + 1 + 1) * 2
+    assertScoreSingleWord board "aaeeiioo" 54 //(7 + 1*2) * 2 * 3 -> 54
+    assertScoreSingleWord board "farces" 30 //((4*2) + 1 + 1 + 3 + 1 + 1) * 2 -> 30
 
 [<Test>]
 let ``Puzzle 1``() =
