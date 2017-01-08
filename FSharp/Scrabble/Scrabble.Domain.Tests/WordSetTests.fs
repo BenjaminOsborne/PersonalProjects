9e9dcf53e46e2c98c8bc275015d265bd64202e78
BenjaminOsborne <ben@accurx.com>
2017-01-08 12:07:54 +0000

    added word pin test

--- FSharp/Scrabble/Scrabble.Domain.Tests/WordSetTests.fs
+++ FSharp/Scrabble/Scrabble.Domain.Tests/WordSetTests.fs
@@ -11,6 +11,23 @@ let wordsByLength (words:WordSet) = words.AllWords |> Seq.groupBy (fun x -> x.Le
 let wordsForLength (wordsByLength:Map<int, string list>) n = match wordsByLength.TryFind n with | Some(l) -> l | None -> []
 
 [<Test>]
+let ``Word Walking 1 letter``() =
+    let createWordSet strings = new WordSet(strings |> Set)
+    let wordSet = createWordSet ["a"; "big"; "hello"; "to"; "my"; "friends"; "again";]
+    
+    let find_a = wordSet.WordsForLengthWithLetters 1 ['b'; 'a'] Map.empty
+    find_a |> should equal ["a"]
+    
+    let find_a_pin = wordSet.WordsForLengthWithLetters 1 ['b'; 'a'] ([(0, 'a')] |> Map)
+    find_a_pin |> should equal ["a"]
+
+    let miss_a = wordSet.WordsForLengthWithLetters 1 ['b'; 'c'] Map.empty
+    miss_a |> should equal []
+    
+    let miss_a_pin = wordSet.WordsForLengthWithLetters 1 ['a';] ([(0, 'x')] |> Map)
+    miss_a_pin |> should equal []
+
+[<Test>]
 let ``Load all words``() =
     let words = WordLoader.LoadAllWords()
     
