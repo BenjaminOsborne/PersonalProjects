9ec94b2a998765db5f31c841cbb2fb769ba50915
BenjaminOsborne <ben@accurx.com>
2017-01-08 12:17:18 +0000

    split files

--- FSharp/Scrabble/Scrabble.Domain.Tests/WordSetTests.fs
+++ FSharp/Scrabble/Scrabble.Domain.Tests/WordSetTests.fs
@@ -3,17 +3,11 @@
 open NUnit.Framework
 open FsUnit
 open Scrabble.Domain
-open System.Collections.Generic
-
-let wordsByLength (words:WordSet) = words.AllWords |> Seq.groupBy (fun x -> x.Length)
-                                                   |> Seq.map (fun (k,vals) -> k, vals |> Seq.toList)
-                                                   |> Map
-let wordsForLength (wordsByLength:Map<int, string list>) n = match wordsByLength.TryFind n with | Some(l) -> l | None -> []
 
 [<Test>]
 let ``Word Walking 1 letter``() =
     let createWordSet strings = new WordSet(strings |> Set)
-    let wordSet = createWordSet ["a"; "big"; "hello"; "to"; "my"; "friends"; "again";]
+    let wordSet = createWordSet ["a"; "big"; "hello"; "to"; "my"; "friends"; "again"; "bin"; "gib"]
     
     let find_a = wordSet.WordsForLengthWithLetters 1 ['b'; 'a'] Map.empty
     find_a |> should equal ["a"]
@@ -26,56 +20,3 @@ let ``Word Walking 1 letter``() =
     
     let miss_a_pin = wordSet.WordsForLengthWithLetters 1 ['a';] ([(0, 'x')] |> Map)
     miss_a_pin |> should equal []
-
-[<Test>]
-let ``Load all words``() =
-    let words = WordLoader.LoadAllWords()
-    
-    let isWord = words.IsWord "ambiguous"
-    let isNotWord = words.IsWord "blaaah"
-    
-    let map = wordsByLength words
-    let count1 = wordsForLength map 1 |> Seq.length
-    let count9 = wordsForLength map 9 |> Seq.length
-    let count25 = wordsForLength map 25 |> Seq.length
-
-    let count_add = words.WordsForLengthWithLetters 3 ['a'; 'd'; 'd'] Map.empty |> Seq.length
-    let count_add_pin = words.WordsForLengthWithLetters 3 ['a'; 'd'; 'd'] ([(1,'a')] |> Map) |> Seq.length
-
-    words.Count |> should equal 267752
-    isWord |> should equal true
-    isNotWord |> should equal false
-    
-    count1 |> should equal 0
-    count9 |> should equal 40727
-    count25 |> should equal 0
-    
-    count_add |> should equal 2
-    count_add_pin |> should equal 1
-
-[<Test>]
-let ``Build stats``() =
-    
-    let words = WordLoader.LoadAllWords()
-    let map = wordsByLength words
-
-    let write (s:string) = System.Console.WriteLine s
-
-    let printMap (map:Map<char,int>) =
-        ['a'..'z'] |> Seq.map (fun c -> let len = match map.TryFind c with | Some(n) -> n | None -> 0
-                                        (c,len))
-                   |> Seq.sortBy (fun (ch,len) -> -len)
-                   |> Seq.iter (fun (ch,len) -> write (ch.ToString() + "\t" + len.ToString()))
-
-    let buildStats length =
-        let byLength = wordsForLength map length
-        let data = [0 .. length-1] |> List.map (fun index -> byLength |> Seq.groupBy (fun s -> s.[index])
-                                                                      |> Seq.map (fun (key,items) -> key, items |> Seq.length)
-                                                                      |> Map)
-        write ("Word Length " + length.ToString())
-        data |> List.iteri (fun nx map -> write "\n"
-                                          write ("Index " + nx.ToString())
-                                          printMap map)
-        write "\n"
-
-    buildStats 5
