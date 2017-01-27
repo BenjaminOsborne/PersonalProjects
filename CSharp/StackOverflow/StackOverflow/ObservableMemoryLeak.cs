46f7e3249804308f9913a46a58290952de72b24a
Benjamin Osborne <ben.i.osborne@gmail.com>
2017-01-27 13:13:55 +0000

    Added StackOverflow sln

---
+++ CSharp/StackOverflow/StackOverflow/ObservableMemoryLeak.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace StackOverflow
+{
+    public class ObservableMemoryLeak
+    {
+        public static void Leak()
+        {
+            IObservable<string> obs = Observable.Generate(1, x => x < 1000 * 1000, x => x + 1, x => x.ToString(), x => TimeSpan.FromMilliseconds(500));
+            obs.Subscribe(x => { /*Do nothing but simply run the observable*/ });
+            Console.ReadLine();
+        }
+
+    }
+}
