1a98d097898e30b5c9e0c3f4639b8d9e7310e503
Benjamin Osborne <ben@accurx.com>
2020-06-26 08:01:54 +0100

    accuRx SignalR talk console and server

---
+++ CSharp/SignalRTalk/ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Please enter your username...");
+            var userName = Console.ReadLine();
+            Console.WriteLine($"{userName} has entered the building.");
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44340/chat")
+                .Build();
+
+            connection.On<string, string>("broadcastMessage", OnSend);
+
+            await connection.StartAsync();
+
+            while (true)
+            {
+                var msg = Console.ReadLine();
+                await connection.InvokeAsync("Send", userName, msg);
+            }
+        }
+
+        private static void OnSend(string name, string message)
+        {
+            Console.WriteLine($"Message from {name}\n{message}\n\n");
+        }
+    }
+}
