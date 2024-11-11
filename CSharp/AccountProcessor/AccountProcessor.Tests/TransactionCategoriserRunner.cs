2eec6b474be5385fa54f193d5635da8e70719e36
Benjamin Osborne <ben@accurx.com>
2024-11-11 13:43:52 +0000

    Add runner

---
+++ CSharp/AccountProcessor/AccountProcessor.Tests/TransactionCategoriserRunner.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.Text.Json;
+
+namespace AccountProcessor.Tests
+{
+    public class TransactionCategoriserRunner
+    {
+        [Test]
+        public void BootstrapJson()
+        {
+            var dir = new DirectoryInfo(Directory.GetCurrentDirectory());
+            while(dir.Name != "CSharp")
+            {
+                dir = dir.Parent!;
+            }
+
+            var outputPath = Path.Combine(dir.FullName, "AccountProcessor", "AccountProcessor", "Components", "Services", "MatchModel.json");
+            var content = JsonSerializer.Serialize(new
+                {
+                    SomeData = new
+                    {
+                        Collection = new[] { "1", "2" }
+                    }
+                },
+                new JsonSerializerOptions { IncludeFields = true, WriteIndented = true });
+            File.WriteAllText(outputPath, content);
+        }
+    }
+}
