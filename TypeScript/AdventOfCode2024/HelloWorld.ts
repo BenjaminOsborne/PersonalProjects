e561a8c788910537138bdc24d7aba733e3fd468d
BenjaminOsborne <ben.i.osborne@gmail.com>
2024-12-16 07:48:05 +0000

    Typescript

---
+++ TypeScript/AdventOfCode2024/HelloWorld.ts
@@ -0,0 +1,11 @@
+let message: string = 'Hello World';
+message += " More";
+console.log(message);
+
+const numbers = [1, 2, 3]; // inferred to type number[]
+numbers.push(4); // no error
+let head: number = numbers[0]; // no error
+let tail: number = numbers[3]; // no error
+console.log(head);
+console.log(tail);
+console.log(numbers);
---
+++ TypeScript/AdventOfCode2024/out/HelloWorld.js
@@ -0,0 +1,9 @@
+var message = 'Hello World';
+message += " More";
+console.log(message);
+var numbers = [1, 2, 3]; // inferred to type number[]
+numbers.push(4); // no error
+var head = numbers[0]; // no error
+console.log(head);
+console.log(numbers);
+//# sourceMappingURL=HelloWorld.js.map
