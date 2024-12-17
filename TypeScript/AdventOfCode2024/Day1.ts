17ea4870676bb96d8fc2eac51db6f6d4bc60d29e
BenjaminOsborne <ben.i.osborne@gmail.com>
2024-12-17 18:47:22 +0000

    day 1 part 2 initial

--- TypeScript/AdventOfCode2024/Day1.ts
+++ TypeScript/AdventOfCode2024/Day1.ts
@@ -1,4 +1,3 @@
-import { Console } from 'console';
 import * as fs from 'fs';
 
 var file = fs.readFileSync('Day1.txt','utf8');
@@ -11,10 +10,28 @@ file.split(/\s+/) //split on all whitespace (newlines AND spaces between numbers
     .forEach((num, nx) => //every other number alternates which array
         (nx % 2 == 0 ? left : right).push(num));
 
+console.info(left);
+
 var orderedLeft = left.sort();
 var orderedRight = right.sort();
 var total = orderedLeft
     .map((val, nx) => val - orderedRight[nx])
     .reduce((agg, val) => agg + Math.abs(val));
 
-console.info("Result: " + total); //2580760 (for Day1.txt)
+console.info("Result (Part1): " + total); //2580760 (for Day1.txt)
+
+//Part 2
+var rightMap = new Map<number, number>();
+orderedRight.forEach(x => {
+    var count = rightMap.get(x);
+    rightMap.set(x, (count || 0) + 1);
+})
+var totalPart2 = orderedLeft
+    .reduce((agg, x) => agg + (x * (rightMap.get(x) || 0)));
+
+var totalPart2_2 = orderedLeft
+    .reduce((agg5, x) => agg5 + (x * (orderedRight.filter(r => r == x).length)));
+
+console.info("Result (Part2): " + totalPart2); //Initial guess: 25368459
+
+console.info("Result (Part2_2): " + totalPart2_2); //Initial guess: 25368459
--- TypeScript/AdventOfCode2024/out/Day1.js
+++ TypeScript/AdventOfCode2024/out/Day1.js
@@ -9,10 +9,25 @@ file.split(/\s+/) //split on all whitespace (newlines AND spaces between numbers
     .forEach(function (num, nx) {
     return (nx % 2 == 0 ? left : right).push(num);
 });
+console.info(left);
 var orderedLeft = left.sort();
 var orderedRight = right.sort();
 var total = orderedLeft
     .map(function (val, nx) { return val - orderedRight[nx]; })
     .reduce(function (agg, val) { return agg + Math.abs(val); });
-console.info("Result: " + total); //2580760 (for Day1.txt)
+console.info("Result (Part1): " + total); //2580760 (for Day1.txt)
+//Part 2
+var rightMap = new Map();
+orderedRight.forEach(function (x) {
+    var count = rightMap.get(x);
+    //console.info("Count: " + count)
+    //console.info("Count2: " + (count || 0))
+    rightMap.set(x, (count || 0) + 1);
+});
+var totalPart2 = orderedLeft
+    .reduce(function (agg, x) { return agg + (x * (rightMap.get(x) || 0)); });
+var totalPart2_2 = orderedLeft
+    .reduce(function (agg5, x) { return agg5 + (x * (orderedRight.filter(function (r) { return r == x; }).length)); });
+console.info("Result (Part2): " + totalPart2); //Initial guess: 25368459
+console.info("Result (Part2_2): " + totalPart2_2); //Initial guess: 25368459
 //# sourceMappingURL=Day1.js.map
