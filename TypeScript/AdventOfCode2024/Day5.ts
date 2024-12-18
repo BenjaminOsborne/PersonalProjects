6fd9b94116ccdfceece72d1a6bc52e24cea68c38
Benjamin Osborne <ben@accurx.com>
2024-12-18 10:57:24 +0000

    Day5 part 2 solved

--- TypeScript/AdventOfCode2024/Day5.ts
+++ TypeScript/AdventOfCode2024/Day5.ts
@@ -3,6 +3,7 @@ var file = fs.readFileSync('Day5.txt','utf8');
 var allLines = file.split('\r\n');
 
 type Rule = { left: number; right: number };
+type ReorderResult = { update: number[]; isReordered: boolean };
 
 var rules = allLines
     .filter(l => l.includes('|'))
@@ -11,33 +12,42 @@ var rules = allLines
         return { left: rule[0], right: rule[1] } as Rule;
     });
 
-var total = allLines
+var updates = allLines
     .filter(l => l.includes(','))
-    .map(l => l.split(',').map(x => Number(x)))
-    .filter(arr => isValidUpdate(arr))
-    .map(arr => arr[Math.floor(arr.length/2)])
+    .map(l => l.split(',').map(x => Number(x)));
+
+    var total = updates
+    .map(arr => getReorderResult(arr, false, 1)) //startIndex:1 as first element cannot break a rule!
+    .filter(x => x.isReordered)
+    .map(x => x.update[Math.floor(x.update.length / 2)])
     .reduce((agg,x) => agg + x, 0);
 
-console.info("Result: " + total);
+console.info("Result: " + total); //Part2: 5448
 
-function isValidUpdate(update: number[])
+function getReorderResult(update: number[], isReordered: boolean, startIndex: number) : ReorderResult
 {
     for(var nx = 1; nx < update.length; nx++)
     {
-        var matchedRules = rules.filter(x => x.left == update[nx]);
+        var currentVal = update[nx];
+        var matchedRules = rules.filter(x => x.left == currentVal);
         if(matchedRules.length == 0)
         {
             continue;
         }
-        var previous = update.slice(0, nx);
-        var brokenRules = matchedRules
-            .filter(rule => previous.filter(x => x == rule.right).length > 0)
-            .length > 0;
-        if(brokenRules)
+        var previous = update.slice(0, nx); //everything up to index (not including!)
+        var failedRules = matchedRules
+            .filter(rule => previous.filter(x => x == rule.right).length > 0);
+        if(failedRules.length == 0)
         {
-            return false;
+            continue;
         }
+        var firstFailed = failedRules[0];
+        var start = previous.filter(x => x != firstFailed.right); //everything except the item ("rule.right") to bump forwards
+        var middle = [currentVal, firstFailed.right]; //the current value with the "rule.right" after it
+        var end = update.slice(nx+1); //everything left in the array after the current value
+        var next = start.concat(middle).concat(end); //combine to next call
+        return getReorderResult(next, true, nx); //startIndex current (NOT nx+1), as element shuffled forwards so "next val" will now be at "nx"
 
     }
-    return true;
+    return { update, isReordered } as ReorderResult;
 }
--- TypeScript/AdventOfCode2024/out/Day5.js
+++ TypeScript/AdventOfCode2024/out/Day5.js
@@ -9,27 +9,35 @@ var rules = allLines
     var rule = l.split('|').map(function (x) { return Number(x); });
     return { left: rule[0], right: rule[1] };
 });
-var total = allLines
+var updates = allLines
     .filter(function (l) { return l.includes(','); })
-    .map(function (l) { return l.split(',').map(function (x) { return Number(x); }); })
-    .filter(function (arr) { return isValidUpdate(arr); })
-    .map(function (arr) { return arr[Math.floor(arr.length / 2)]; })
+    .map(function (l) { return l.split(',').map(function (x) { return Number(x); }); });
+var total = updates
+    .map(function (arr) { return getReorderResult(arr, false, 1); }) //startIndex:1 as first element cannot break a rule!
+    .filter(function (x) { return x.isReordered; })
+    .map(function (x) { return x.update[Math.floor(x.update.length / 2)]; })
     .reduce(function (agg, x) { return agg + x; }, 0);
-console.info("Result: " + total);
-function isValidUpdate(update) {
+console.info("Result: " + total); //Part2: 5448
+function getReorderResult(update, isReordered, startIndex) {
     for (var nx = 1; nx < update.length; nx++) {
-        var matchedRules = rules.filter(function (x) { return x.left == update[nx]; });
+        var currentVal = update[nx];
+        var matchedRules = rules.filter(function (x) { return x.left == currentVal; });
         if (matchedRules.length == 0) {
             continue;
         }
-        var previous = update.slice(0, nx);
-        var brokenRules = matchedRules
-            .filter(function (rule) { return previous.filter(function (x) { return x == rule.right; }).length > 0; })
-            .length > 0;
-        if (brokenRules) {
-            return false;
+        var previous = update.slice(0, nx); //everything up to index (not including!)
+        var failedRules = matchedRules
+            .filter(function (rule) { return previous.filter(function (x) { return x == rule.right; }).length > 0; });
+        if (failedRules.length == 0) {
+            continue;
         }
+        var firstFailed = failedRules[0];
+        var start = previous.filter(function (x) { return x != firstFailed.right; }); //everything except the item ("rule.right") to bump forwards
+        var middle = [currentVal, firstFailed.right]; //the current value with the "rule.right" after it
+        var end = update.slice(nx + 1); //everything left in the array after the current value
+        var next = start.concat(middle).concat(end); //combine to next call
+        return getReorderResult(next, true, nx); //startIndex current (NOT nx+1), as element shuffled forwards so "next val" will now be at "nx"
     }
-    return true;
+    return { update: update, isReordered: isReordered };
 }
 //# sourceMappingURL=Day5.js.map
