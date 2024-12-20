875ff67cd3f194f1bb339f7cd9356e8acc191c26
Benjamin Osborne <ben@accurx.com>
2024-12-20 12:43:41 +0000

    revert day 11 to previous impl.

--- TypeScript/AdventOfCode2024/Day11.ts
+++ TypeScript/AdventOfCode2024/Day11.ts
@@ -1,84 +1,37 @@
 import './Globals'
 import FileHelper from './FileHelper';
-import { error } from 'console';
 
-type Link = { val: number, next: Link };
-
+type Num = { num: number, asString: string };
 var loaded = FileHelper.LoadFile('Inputs\\Day11.txt')
-    .split(' ');
+    .split(' ')
+    .map(x => ({ num: Number(x), asString: x } as Num));
 
-var headLink: Link = undefined;
-var totalNums = loaded.length;
-
-for(var n = loaded.length-1; n >= 0; n--)
-{
-    var sNum = loaded[n];
-    headLink = { val: Number(sNum), next: headLink } as Link;
-}
+var input = loaded;
 
 for(var n = 0; n < 75; n++)
 {
-    var local = headLink;
-    while(local !== undefined)
-    {
-        local = blink(local);
-    }
-    console.info("Loop: " + (n+1) + ". Count: " + totalNums)
+    input = input.flatMap(getNext);
+    console.info("Loop: " + (n+1) + ". Count: " + input.length)
 }
 
-console.info("Result: " + totalNums); //Part1: 186424 (25 loops)
+console.info("Result: " + input.length)
 
-function blink(link : Link) : Link
+function getNext(num : Num) : Num[]
 {
-    if(link === undefined)
+    if(num.num == 0)
     {
-        return undefined;
+        return [{ num: 1, asString: "1" }]
     }
-
-    if(link.val == 0)
-    {
-        link.val = 1;
-        return link.next;
-    }
-    const numLen = getDigits(link.val);
+    const numLen = num.asString.length;
     if(numLen %2 == 0)
     {
-        var str = link.val.toString();
-        var num1 = Number(str.slice(0, numLen / 2));
-        var num2 = Number(str.slice(numLen / 2));
-        
-        link.val = num1;
-        var rePoint = link.next;
-        link.next = { val: num2, next: rePoint } as Link;
-        totalNums += 1;
-        return rePoint;
+        var num1 = Number(num.asString.slice(0, numLen / 2));
+        var num2 = Number(num.asString.slice(numLen / 2));
+        return [
+            { num: num1, asString: num1.toString() },
+            { num: num2, asString: num2.toString() }
+        ]
     }
-
-    link.val *= 2024;
-    return link.next;
-}
-
-function getDigits(n: number)
-{
-    if (n < 10) return 1;
-    if (n < 100) return 2;
-    if (n < 1000) return 3;
-    if (n < 10000) return 4;
-    if (n < 100000) return 5;
-    if (n < 1000000) return 6;
-    if (n < 10000000) return 7;
-    if (n < 100000000) return 8;
-    if (n < 1000000000) return 9;
-    if (n < 10000000000) return 10;
-    if (n < 100000000000) return 11;
-    if (n < 1000000000000) return 12;
-    if (n < 10000000000000) return 13;
-    if (n < 100000000000000) return 14;
-    if (n < 1000000000000000) return 15;
-    if (n < 10000000000000000) return 16;
-    if (n < 100000000000000000) return 17;
-    if (n < 1000000000000000000) return 18;
-    if (n < 10000000000000000000) return 19;
-    
-    throw new Error("UNHANDLED NUMBER: " + n);
+    var next = num.num * 2024;
+    return [{ num: next, asString: next.toString() },]
 }
--- TypeScript/AdventOfCode2024/out/Day11.js
+++ TypeScript/AdventOfCode2024/out/Day11.js
@@ -3,82 +3,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("./Globals");
 var FileHelper_1 = require("./FileHelper");
 var loaded = FileHelper_1.default.LoadFile('Inputs\\Day11.txt')
-    .split(' ');
-var headLink = undefined;
-var totalNums = loaded.length;
-for (var n = loaded.length - 1; n >= 0; n--) {
-    var sNum = loaded[n];
-    headLink = { val: Number(sNum), next: headLink };
-}
+    .split(' ')
+    .map(function (x) { return ({ num: Number(x), asString: x }); });
+var input = loaded;
 for (var n = 0; n < 75; n++) {
-    var local = headLink;
-    while (local !== undefined) {
-        local = blink(local);
-    }
-    console.info("Loop: " + (n + 1) + ". Count: " + totalNums);
+    input = input.flatMap(getNext);
+    console.info("Loop: " + (n + 1) + ". Count: " + input.length);
 }
-console.info("Result: " + totalNums); //Part1: 186424 (25 loops)
-function blink(link) {
-    if (link === undefined) {
-        return undefined;
+console.info("Result: " + input.length);
+function getNext(num) {
+    if (num.num == 0) {
+        return [{ num: 1, asString: "1" }];
     }
-    if (link.val == 0) {
-        link.val = 1;
-        return link.next;
-    }
-    var numLen = getDigits(link.val);
+    var numLen = num.asString.length;
     if (numLen % 2 == 0) {
-        var str = link.val.toString();
-        var num1 = Number(str.slice(0, numLen / 2));
-        var num2 = Number(str.slice(numLen / 2));
-        link.val = num1;
-        var rePoint = link.next;
-        link.next = { val: num2, next: rePoint };
-        totalNums += 1;
-        return rePoint;
+        var num1 = Number(num.asString.slice(0, numLen / 2));
+        var num2 = Number(num.asString.slice(numLen / 2));
+        return [
+            { num: num1, asString: num1.toString() },
+            { num: num2, asString: num2.toString() }
+        ];
     }
-    link.val *= 2024;
-    return link.next;
-}
-function getDigits(n) {
-    if (n < 10)
-        return 1;
-    if (n < 100)
-        return 2;
-    if (n < 1000)
-        return 3;
-    if (n < 10000)
-        return 4;
-    if (n < 100000)
-        return 5;
-    if (n < 1000000)
-        return 6;
-    if (n < 10000000)
-        return 7;
-    if (n < 100000000)
-        return 8;
-    if (n < 1000000000)
-        return 9;
-    if (n < 10000000000)
-        return 10;
-    if (n < 100000000000)
-        return 11;
-    if (n < 1000000000000)
-        return 12;
-    if (n < 10000000000000)
-        return 13;
-    if (n < 100000000000000)
-        return 14;
-    if (n < 1000000000000000)
-        return 15;
-    if (n < 10000000000000000)
-        return 16;
-    if (n < 100000000000000000)
-        return 17;
-    if (n < 1000000000000000000)
-        return 18;
-    if (n < 10000000000000000000)
-        return 19;
-    throw new Error("UNHANDLED NUMBER: " + n);
+    var next = num.num * 2024;
+    return [{ num: next, asString: next.toString() },];
 }
 //# sourceMappingURL=Day11.js.map
