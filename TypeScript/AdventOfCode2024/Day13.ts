fef00b5d8017f43c28d1c128c77e8d7675c28a78
BenjaminOsborne <ben.i.osborne@gmail.com>
2024-12-21 01:29:33 +0000

    linear equation... a bit off

--- TypeScript/AdventOfCode2024/Day13.ts
+++ TypeScript/AdventOfCode2024/Day13.ts
@@ -1,10 +1,10 @@
 import './Globals'
 import FileHelper from './FileHelper';
 
-var lines = FileHelper.LoadFileLines('Inputs\\Day13.txt');
+var lines = FileHelper.LoadFileLines('Inputs\\Day13_Test.txt');
 
-type Button = { MoveX: number, MoveY: number };
-type Setup = { PrizeX: number, PrizeY: number, ButA: Button, ButB : Button };
+type Button = { MoveX: bigint, MoveY: bigint };
+type Setup = { PrizeX: bigint, PrizeY: bigint, ButA: Button, ButB : Button };
 
 var tokens = lines
     .map((x,nx) => ({ Line: x, Block: Math.floor(nx / 3) }))
@@ -14,37 +14,60 @@ var tokens = lines
         const bA = loadButton(x.Items[0].Line);
         const bB = loadButton(x.Items[1].Line);
         const split = x.Items[2].Line.replace(", Y", "").split('=');
-        return { PrizeX: Number(split[1]), PrizeY: Number(split[2]), ButA: bA, ButB: bB } as Setup
+        var offset: bigint = BigInt(10000000000000);
+        offset = BigInt(0);
+        return { PrizeX: BigInt(split[1]) + offset, PrizeY: BigInt(split[2]) + offset, ButA: bA, ButB: bB } as Setup
     })
     .map(getMinTokens)
-    .sum();
+    .reduce((agg,x) => agg + x, BigInt(0));
 console.log("Result: " + tokens)
 
-function getMinTokens(s: Setup) : number
+function getMinTokens(s: Setup) : bigint
 {
-    var maxA = Math.floor(Math.min(s.PrizeX / s.ButA.MoveX, s.PrizeY / s.ButA.MoveY));
+    var unit: bigint = BigInt(1);
+    var numAPress = ((s.PrizeX / s.ButA.MoveX) -
+        (s.PrizeY * (s.ButB.MoveX / (s.ButA.MoveY * s.ButA.MoveX))))
+        /
+        (unit - (s.ButA.MoveY * s.ButB.MoveX) / (s.ButB.MoveY * s.ButA.MoveX));
+    var numBPress = ((s.PrizeY / s.ButB.MoveY) - 
+        (s.PrizeX * (s.ButA.MoveY / (s.ButB.MoveX * s.ButB.MoveY))))
+        /
+        (unit - (s.ButB.MoveX * s.ButA.MoveY) / (s.ButA.MoveX * s.ButB.MoveY));
 
-    var possible = 0;
-    for(var nA = 0; nA <= maxA; nA++)
+    console.info("A Press: " + numAPress);
+    console.info("B Press: " + numBPress);
+
+    if(numAPress < 0 || numBPress < 0)
     {
-        var gapX = s.PrizeX - nA * s.ButA.MoveX;
-        var gapY = s.PrizeY - nA * s.ButA.MoveY;
-        if(gapX % s.ButB.MoveX == 0)
-        {
-            var pressB = gapX / s.ButB.MoveX;
-            if(gapY == pressB * s.ButB.MoveY)
-            {
-                var score = 3 * nA + pressB;
-                possible = possible == 0 ? score : Math.min(possible, score);
-            }
-        }
+        return BigInt(0);
     }
-
-    return possible;
+    var three: bigint = BigInt(3);
+    return numAPress * three + numBPress;
 }
 
 function loadButton(line: string) : Button
 {
     const split = line.replace(", Y", "").split('+');
-    return { MoveX: Number(split[1]), MoveY: Number(split[2]) }
+    return { MoveX: BigInt(split[1]), MoveY: BigInt(split[2]) }
 }
+
+/*
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=10000000008400, Y=10000000005400
+
+A * 94 + B * 22 = PX
+A * 34 + B * 67 = PY
+
+A = PX / 94 - B * 22 / 94
+B = PY / 67 - A * 34 / 67
+
+B = PY / 67 - (PX / 94 - B * 22 / 94) * 34 / 67
+B = PY / 67 - (PX / 94) * (34 / 67) + B * (22 / 94) * (34 / 67)
+B = (PY / 67 - (PX * 34) / (94 * 67)) / (1 - (22 * 34) / (94 * 67)
+
+A = PX / 94 - ((PY / 67) - A * (34 / 67)) * 22 / 94
+A = PX / 94 - (PY / 67) * (22 / 94) + A * (34 / 67) * (22 / 94)
+A = (PX / 94 - (PY * 22) / (67 * 94)) / (1 - (34 * 22) / (67 * 94))
+
+*/
--- TypeScript/AdventOfCode2024/out/Day13.js
+++ TypeScript/AdventOfCode2024/out/Day13.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("./Globals");
 var FileHelper_1 = require("./FileHelper");
-var lines = FileHelper_1.default.LoadFileLines('Inputs\\Day13.txt');
+var lines = FileHelper_1.default.LoadFileLines('Inputs\\Day13_Test.txt');
 var tokens = lines
     .map(function (x, nx) { return ({ Line: x, Block: Math.floor(nx / 3) }); })
     .groupBy(function (x) { return x.Block; })
@@ -10,29 +10,53 @@ var tokens = lines
     var bA = loadButton(x.Items[0].Line);
     var bB = loadButton(x.Items[1].Line);
     var split = x.Items[2].Line.replace(", Y", "").split('=');
-    return { PrizeX: Number(split[1]), PrizeY: Number(split[2]), ButA: bA, ButB: bB };
+    var offset = BigInt(10000000000000);
+    offset = BigInt(0);
+    return { PrizeX: BigInt(split[1]) + offset, PrizeY: BigInt(split[2]) + offset, ButA: bA, ButB: bB };
 })
     .map(getMinTokens)
-    .sum();
+    .reduce(function (agg, x) { return agg + x; }, BigInt(0));
 console.log("Result: " + tokens);
 function getMinTokens(s) {
-    var maxA = Math.floor(Math.min(s.PrizeX / s.ButA.MoveX, s.PrizeY / s.ButA.MoveY));
-    var possible = 0;
-    for (var nA = 0; nA <= maxA; nA++) {
-        var gapX = s.PrizeX - nA * s.ButA.MoveX;
-        var gapY = s.PrizeY - nA * s.ButA.MoveY;
-        if (gapX % s.ButB.MoveX == 0) {
-            var pressB = gapX / s.ButB.MoveX;
-            if (gapY == pressB * s.ButB.MoveY) {
-                var score = 3 * nA + pressB;
-                possible = possible == 0 ? score : Math.min(possible, score);
-            }
-        }
+    var unit = BigInt(1);
+    var numAPress = ((s.PrizeX / s.ButA.MoveX) -
+        (s.PrizeY * (s.ButB.MoveX / (s.ButA.MoveY * s.ButA.MoveX))))
+        /
+            (unit - (s.ButA.MoveY * s.ButB.MoveX) / (s.ButB.MoveY * s.ButA.MoveX));
+    var numBPress = ((s.PrizeY / s.ButB.MoveY) -
+        (s.PrizeX * (s.ButA.MoveY / (s.ButB.MoveX * s.ButB.MoveY))))
+        /
+            (unit - (s.ButB.MoveX * s.ButA.MoveY) / (s.ButA.MoveX * s.ButB.MoveY));
+    console.info("A Press: " + numAPress);
+    console.info("B Press: " + numBPress);
+    if (numAPress < 0 || numBPress < 0) {
+        return BigInt(0);
     }
-    return possible;
+    var three = BigInt(3);
+    return numAPress * three + numBPress;
 }
 function loadButton(line) {
     var split = line.replace(", Y", "").split('+');
-    return { MoveX: Number(split[1]), MoveY: Number(split[2]) };
+    return { MoveX: BigInt(split[1]), MoveY: BigInt(split[2]) };
 }
+/*
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=10000000008400, Y=10000000005400
+
+A * 94 + B * 22 = PX
+A * 34 + B * 67 = PY
+
+A = PX / 94 - B * 22 / 94
+B = PY / 67 - A * 34 / 67
+
+B = PY / 67 - (PX / 94 - B * 22 / 94) * 34 / 67
+B = PY / 67 - (PX / 94) * (34 / 67) + B * (22 / 94) * (34 / 67)
+B = (PY / 67 - (PX * 34) / (94 * 67)) / (1 - (22 * 34) / (94 * 67)
+
+A = PX / 94 - ((PY / 67) - A * (34 / 67)) * 22 / 94
+A = PX / 94 - (PY / 67) * (22 / 94) + A * (34 / 67) * (22 / 94)
+A = (PX / 94 - (PY * 22) / (67 * 94)) / (1 - (34 * 22) / (67 * 94))
+
+*/ 
 //# sourceMappingURL=Day13.js.map
