880b2da43bb992fdd6960576a4ef657d49f937d4
Ben Osborne <ben.i.osborne@gmail.com>
2025-03-15 11:16:25 +0000

    Initial template

---
+++ CSharp/AccountProcessor/MudBlazorTemplate/MudBlazorTemplate/Controllers/WeatherController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Immutable;
+using Microsoft.AspNetCore.Mvc;
+using MudBlazorTemplate.Client.DTOs;
+
+namespace MudBlazorTemplate.Controllers;
+
+public class WeatherController : ControllerBase
+{
+    [HttpGet]
+    public IReadOnlyList<WeatherForecast> Get()
+    {
+        var startDate = DateOnly.FromDateTime(DateTime.Now);
+        var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+        return Enumerable.Range(1, 5)
+            .Select(index => new WeatherForecast(
+                Date: startDate.AddDays(index),
+                TemperatureC: Random.Shared.Next(-20, 55),
+                Summary: summaries[Random.Shared.Next(summaries.Length)]))
+            .ToImmutableList();
+    }
+}
