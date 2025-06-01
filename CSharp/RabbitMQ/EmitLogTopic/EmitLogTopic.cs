c769442a683a33b28b9cdcf492a15d4c5442eef4
Ben Osborne <ben.i.osborne@gmail.com>
2025-06-01 22:29:52 +0100

    [RMQ] Topics

---
+++ CSharp/RabbitMQ/EmitLogTopic/EmitLogTopic.cs
@@ -0,0 +1,10 @@
+﻿using Shared;
+
+await using var wrapper = await ChannelHelper.CreateExchangeAsync(
+    exchangeName: "topic_logs",
+    exchangeType: RabbitMQ.Client.ExchangeType.Topic);
+
+var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+var message = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+await wrapper.SendMessageAsync(routingKey: routingKey, message: message);
+Console.WriteLine($" [x] Sent '{routingKey}':'{message}'");
---
+++ CSharp/RabbitMQ/ReceiveLogsTopic/ReceiveLogsTopic.cs
@@ -0,0 +1,30 @@
+﻿using Shared;
+using System.Text;
+
+if (args.Length < 1)
+{
+    Console.Error.WriteLine("Usage: {0} [binding_key...]",
+        Environment.GetCommandLineArgs()[0]);
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+    Environment.ExitCode = 1;
+    return;
+}
+
+await using var wrapper = await ChannelHelper.CreateExchangeAsync(
+    exchangeName: "topic_logs",
+    exchangeType: RabbitMQ.Client.ExchangeType.Topic);
+
+await wrapper.CreateQueueAndConsumeAsync(
+    routingKeys: args,
+    fnOnReceivedAsync: (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        var routingKey = ea.RoutingKey;
+        Console.WriteLine($" [x] Received '{routingKey}':'{message}'");
+        return Task.CompletedTask;
+    });
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
