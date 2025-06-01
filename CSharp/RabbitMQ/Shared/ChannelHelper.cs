9d212cfb0e969b9ea053fb18f071cbf26382a3a6
Ben Osborne <ben.i.osborne@gmail.com>
2025-06-01 14:06:54 +0100

    [RMQ] Refac durable

--- CSharp/RabbitMQ/Shared/ChannelHelper.cs
+++ CSharp/RabbitMQ/Shared/ChannelHelper.cs
@@ -6,7 +6,7 @@ namespace Shared;
 
 public static class ChannelHelper
 {
-    public class ChannelWrapper(IChannel channel, QueueDeclareOk queue, IReadOnlyList<IAsyncDisposable> disposables) : IAsyncDisposable
+    public class ChannelWrapper(IChannel channel, QueueDeclareOk queue, bool isDurable, IReadOnlyList<IAsyncDisposable> disposables) : IAsyncDisposable
     {
         public async ValueTask DisposeAsync()
         {
@@ -19,26 +19,45 @@ public static class ChannelHelper
         public async Task SendMessageAsync(string message)
         {
             var body = Encoding.UTF8.GetBytes(message);
-            await channel.BasicPublishAsync(exchange: string.Empty, routingKey: queue.QueueName, body: body);
+            await channel.BasicPublishAsync(exchange: string.Empty,
+                routingKey: queue.QueueName,
+                basicProperties: new BasicProperties { Persistent = isDurable },
+                mandatory: true,
+                body: body);
             Console.WriteLine($" [x] Sent {message}");
         }
 
-        public async Task BasicConsumeAsync(AsyncEventHandler<BasicDeliverEventArgs> fnOnReceivedAsync)
+        public async Task BasicConsumeAsync(Func<AsyncEventingBasicConsumer, BasicDeliverEventArgs, Task> fnOnReceivedAsync,
+            bool limitTo1Prefetch = true,
+            bool autoAck = true)
         {
+            if (limitTo1Prefetch)
+            {
+                await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false); //QoS is "Quality of Service"
+            }
+
             var consumer = new AsyncEventingBasicConsumer(channel);
-            consumer.ReceivedAsync += fnOnReceivedAsync;
-            await channel.BasicConsumeAsync(queue.QueueName, autoAck: true, consumer: consumer);
+            consumer.ReceivedAsync += async (sender, ea) =>
+            {
+                await fnOnReceivedAsync((AsyncEventingBasicConsumer) sender, ea);
+                
+                if (autoAck == false)
+                {
+                    await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+                }
+            };
+            await channel.BasicConsumeAsync(queue.QueueName, autoAck: autoAck, consumer: consumer);
         }
     }
 
-    public static async Task<ChannelWrapper> CreateAsync()
+    public static async Task<ChannelWrapper> CreateAsync(bool durable = true)
     {
         var factory = new ConnectionFactory { HostName = "localhost", UserName = "rabbitmq_admin_user", Password = "bg0mXyNAOD1vay8VP" };
         var connection = await factory.CreateConnectionAsync();
         var channel = await connection.CreateChannelAsync();
 
-        var queue = await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
-        return new(channel, queue, [channel, connection]);
+        var queue = await channel.QueueDeclareAsync(queue: "Example_2", durable: durable, exclusive: false, autoDelete: false, arguments: null);
+        return new(channel, queue, durable, [channel, connection]);
     }
 
 }
