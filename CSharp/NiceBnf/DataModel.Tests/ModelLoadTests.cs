8863712c8f6122ec2d713da94d05d0347cfbd952
Ben Osborne <ben.i.osborne@gmail.com>
2026-03-08 14:35:45 +0000

    [NiceBnf] Add initial test

---
+++ CSharp/NiceBnf/DataModel.Tests/ModelLoadTests.cs
@@ -0,0 +1,12 @@
+﻿namespace DataModel.Tests;
+
+public class ModelLoadTests
+{
+    [Test]
+    public async Task Check_load()
+    {
+        var all = await new ModelLoader().LoadDrugsAsync();
+        var found = all.Single(x => x.Name == "Abacavir with lamivudine");
+        Assert.That(found.Indications.Count, Is.EqualTo(1));
+    }
+}
